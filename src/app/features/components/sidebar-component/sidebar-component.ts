c901823ff73ad8bc96b41c47627b97d23bf636e1
Ruben Castañeda <portela652@gmail.com>
2026-05-22 22:38:11 -0500

    CONFIG:Se agrega la dependencia de fontawesome al proyecto

--- src/app/features/components/sidebar-component/sidebar-component.ts
+++ src/app/features/components/sidebar-component/sidebar-component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar-component',
   standalone: true,
-  imports: [RouterModule,CommonModule],
+  imports: [RouterModule,CommonModule,FontAwesomeModule],
   templateUrl: './sidebar-component.html',
   styleUrl: './sidebar-component.css',
 })
 export class SidebarComponent {
-
+  faUser = faUser;
   sidebarOpen = true;
 
   toggleSidebar() {
